bdabf563b93da56d6ac85ab35c0241f881180d16
Jessica Thompson <jlthomps@usgs.gov>
2013-05-17 15:49:55 -0500

    fixed to use thresh from peakthresh

--- R/RProjects/HITHATStats/R/dh24.R
+++ R/RProjects/HITHATStats/R/dh24.R
@@ -4,17 +4,15 @@
 #' calculates the mean duration of flow events below the 60th percentile for the entire record
 #' 
 #' @param qfiletempf data frame containing a "discharge" column containing daily flow values
+#' @param thresh numeric containing 1.67-year flood threshold calculated by getPeakThresh
 #' @return dh24 list containing the mean annual flood days for the given data frame
 #' @export
 #' @examples
 #' load_data<-paste(system.file(package="HITHATStats"),"/data/obs_data.csv",sep="")
 #' qfiletempf<-read.csv(load_data)
 #' dh24(qfiletempf)
-dh24 <- function(qfiletempf) {
-  isolateq <- qfiletempf$discharge
-  sortq <- sort(isolateq)
-  frank <- floor(findrank(length(sortq), 0.40))
-  lfcrit <- sortq[frank]
+dh24 <- function(qfiletempf, thresh) {
+  lfcrit <- thresh
   noyears <- aggregate(qfiletempf$discharge, list(qfiletempf$wy_val), 
                        FUN = median, na.rm=TRUE)
   colnames(noyears) <- c("Year", "momax")
