37e281488566088d01302998922442da59935562
Jessica Thompson <jlthomps@usgs.gov>
2013-05-17 15:50:19 -0500

    fixed to use wy

--- R/RProjects/HITHATStats/R/meanflowbyyear.R
+++ R/RProjects/HITHATStats/R/meanflowbyyear.R
@@ -11,7 +11,7 @@
 #' qfiletempf<-read.csv(load_data)
 #' meanflowbyyear(qfiletempf)
 meanflowbyyear <- function(qfiletempf) {
-  meanflowbyyear<-aggregate(qfiletempf$discharge, list(qfiletempf$year_val), 
+  meanflowbyyear<-aggregate(qfiletempf$discharge, list(qfiletempf$wy_val), 
                             mean, na.rm=TRUE)
   colnames(meanflowbyyear) <- c("Year", "meanq")
   return(meanflowbyyear)
