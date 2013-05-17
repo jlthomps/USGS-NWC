32800bcd40959c86c7ae14184293887a538e9183
Jessica Thompson <jlthomps@usgs.gov>
2013-05-17 15:49:04 -0500

    fixed to use thresh from peakthresh

--- R/RProjects/HITHATStats/R/dh22.R
+++ R/RProjects/HITHATStats/R/dh22.R
@@ -4,17 +4,15 @@
 #' calculates the mean of annual median days between flood events for the entire record
 #' 
 #' @param qfiletempf data frame containing a "discharge" column containing daily flow values
+#' @param thresh numeric containing 1.67-year flood threshold calculated by getPeakThresh
 #' @return dh22 numeric containing the mean of annual median flood intervals for the given data frame
 #' @export
 #' @examples
 #' load_data<-paste(system.file(package="HITHATStats"),"/data/obs_data.csv",sep="")
 #' qfiletempf<-read.csv(load_data, stringsAsFactors=FALSE)
 #' dh22(qfiletempf)
-dh22 <- function(qfiletempf) {
-  isolateq <- qfiletempf$discharge
-  sortq <- sort(isolateq)
-  frank <- floor(findrank(length(sortq), 0.40))
-  lfcrit <- sortq[frank]
+dh22 <- function(qfiletempf, thresh) {
+  lfcrit <- thresh
   noyears <- aggregate(qfiletempf$discharge, list(qfiletempf$wy_val), 
                        FUN = median, na.rm=TRUE)
   colnames(noyears) <- c("Year", "momax")
